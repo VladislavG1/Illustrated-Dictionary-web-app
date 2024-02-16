69fcd3a2b530b8cda31b33b56c03b882c212d2a9
GlukhovVI <example@mail.com>
2024-02-16 19:34:56 +0500

    Main app complete, pages index and about

--- main/views.py
+++ main/views.py
@@ -2,7 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):  
-    return render(request, 'main/index.html')
+    context = {
+        'title': "Илюстрированный словарь"
+    }
+    return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': "О нас - Илюстрированный словарь"
+    }
+    return render(request, 'main/about.html', context)
