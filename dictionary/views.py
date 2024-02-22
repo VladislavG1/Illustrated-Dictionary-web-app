38b295b9e02bc1b74aaca2db25ef156436564014
GlukhovVI <example@mail.com>
2024-02-22 18:18:57 +0500

    Before create Word page

--- dictionary/views.py
+++ dictionary/views.py
@@ -4,13 +4,13 @@ from .models import Group
 def catalog(request):
     data_group = Group.objects.all()
     context = {
-        'title': "Содержание - Илюстрированный словарь",
+        'title': "Содержание - Иллюстрированный словарь",
         'groups': data_group
     }
     return render(request, 'dictionary/catalog.html', context)
 
 def word(request):
     context = {
-        'title': "Слово - Илюстрированный словарь"
+        'title': "Слово - Иллюстрированный словарь"
     }
     return render(request, 'dictionary/word.html', context)
--- main/views.py
+++ main/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 
 def index(request):  
     context = {
-        'title': "Илюстрированный словарь"
+        'title': "Иллюстрированный словарь"
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
     context = {
-        'title': "О нас - Илюстрированный словарь"
+        'title': "О нас - Иллюстрированный словарь"
     }
     return render(request, 'main/about.html', context)
